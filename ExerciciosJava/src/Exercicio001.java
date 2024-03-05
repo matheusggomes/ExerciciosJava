47fc7dba6db7576fc78b2d6984dfc700cf8b9671
Matheus Gonçalves Gomes <matheusggomes.souza@gmail.com>
2024-03-05 15:37:42 -0300

    Criação do Exercicio002.java

--- ExerciciosJava/src/Exercicio001.java
+++ ExerciciosJava/src/Exercicio001.java
@@ -2,7 +2,7 @@
 
 Faça um programa para ler dois valores inteiros,
 e depois mostrar na tela a soma desses números
-com uma mensagem explicativa
+com uma mensagem explicativa.
 
 */
 
---
+++ ExerciciosJava/src/Exercicio002.java
@@ -0,0 +1,33 @@
+/*
+ 
+Faça um programa para ler o valor do raio de um círculo, e
+depois mostrar o valor da área deste círculo com quatro
+casas decimais.
+
+Fórmula da área: area = π . raio²
+Considere o valor de π = 3.14159 
+  
+*/
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio002 {
+
+	public static void main(String[] args) {
+		double raioCirculo, areaCirculo, pi = 3.14159;
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Digite o valor do raio do círculo: ");
+		raioCirculo = sc.nextDouble();
+
+		areaCirculo = pi * Math.pow(raioCirculo, 2);
+
+		System.out.printf("A = %.4f%n", areaCirculo);
+
+		sc.close();
+	}
+
+}
