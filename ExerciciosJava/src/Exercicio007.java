ff22d7105d72268f1ce5a361b7bcc1b90e906cc6
Matheus Gonçalves Gomes <matheusggomes.souza@gmail.com>
2024-03-10 10:51:02 -0300

    Criação do Exercicio007.java

---
+++ ExerciciosJava/src/Exercicio007.java
@@ -0,0 +1,30 @@
+/*
+  
+ Fazer um programa para ler um número inteiro, e
+ depois dizer se este número é negativo ou não. 
+  
+ */
+
+import java.util.Scanner;
+
+public class Exercicio007 {
+
+	public static void main(String[] args) {
+		int num;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite um número inteiro: ");
+		num = sc.nextInt();
+		
+		if (num < 0) {
+			System.out.println("NEGATIVO");
+		}else {
+			System.out.println("NÃO NEGATIVO");
+		}
+		
+		sc.close();
+
+	}
+
+}
