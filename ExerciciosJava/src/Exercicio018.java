a205596d968921bcc0153e701c1aeffe4ba6e266
Matheus Gonçalves Gomes <matheusggomes.souza@gmail.com>
2024-03-23 12:19:46 -0300

    Criação do Exercicio018.java

---
+++ ExerciciosJava/src/Exercicio018.java
@@ -0,0 +1,27 @@
+/*
+ 
+Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, 
+um valor por linha, inclusive o X, se for o caso. 
+
+*/
+
+import java.util.Scanner;
+
+public class Exercicio018 {
+
+	public static void main(String[] args) {
+		int x;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite um número entre 1 e 1000: ");
+		x = sc.nextInt();
+		
+		for (int i = 1; i<=x; i+=2) {
+			System.out.println(i);
+		}
+		
+		sc.close();
+	}
+
+}
