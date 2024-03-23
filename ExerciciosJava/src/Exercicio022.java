01efdc40b7b123a2eca0fb8505313f4c5c91336a
Matheus Gonçalves Gomes <matheusggomes.souza@gmail.com>
2024-03-23 12:23:17 -0300

    Criação do Exercicio022.java

---
+++ ExerciciosJava/src/Exercicio022.java
@@ -0,0 +1,34 @@
+/*
+
+Ler um valor N. Calcular e escrever seu respectivo fatorial. 
+Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+Lembrando que, por definição, fatorial de 0 é 1.
+
+*/
+
+import java.util.Scanner;
+
+public class Exercicio022 {
+
+	public static void main(String[] args) {
+		int n, fatorial = 1;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("-----Fatorial-----\nDigite um número inteiro: ");
+		n = sc.nextInt();
+		
+		if (n >= 0) {
+			for (int i=n; i>0; i--) {
+				fatorial = fatorial * i;
+			}
+			System.out.println(n + "! = " + fatorial);
+		} else {
+			System.out.println("Digite um número positivo");
+		}
+		
+		sc.close();
+
+	}
+
+}
