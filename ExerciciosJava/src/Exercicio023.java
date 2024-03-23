6fd831983cc290494bd76f4517950840ad4c5fce
Matheus Gonçalves Gomes <matheusggomes.souza@gmail.com>
2024-03-23 12:23:41 -0300

    Criação do Exercicio023.java

---
+++ ExerciciosJava/src/Exercicio023.java
@@ -0,0 +1,28 @@
+/*
+ 
+Ler um número inteiro N e calcular todos os seus divisores.
+
+*/
+
+import java.util.Scanner;
+
+public class Exercicio023 {
+
+	public static void main(String[] args) {
+		int n;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite um número inteiro: ");
+		n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+			if (n % i == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		sc.close();
+	}
+
+}
