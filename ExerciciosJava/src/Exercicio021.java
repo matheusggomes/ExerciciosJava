d4a6d10f6a115276b9f3d2833d4be7bdb1d6fcec
Matheus Gonçalves Gomes <matheusggomes.souza@gmail.com>
2024-03-23 12:22:51 -0300

    Criação do Exercicio021.java

---
+++ ExerciciosJava/src/Exercicio021.java
@@ -0,0 +1,42 @@
+/*
+ 
+Fazer um programa para ler um número N. Depois leia N pares de números e mostre a 
+divisão do primeiro pelo segundo. Se o denominador for igual a zero, mostrar a 
+mensagem "divisao impossivel". 
+ 
+*/
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio021 {
+
+	public static void main(String[] args) {
+		int n;
+		double a, b, divisao;
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Quantos pares deseja digitar? ");
+		n = sc.nextInt();
+		
+		for (int i=0; i<n; i++) {
+			
+			System.out.print(i+1 + "º par (digite dois números separados por espaço): ");
+			a = sc.nextInt();
+			b = sc.nextInt();
+			
+			if (b==0) {
+				System.out.println("Divisão Impossível");
+			} else {
+				divisao = a/b;
+				System.out.printf("%.1f\n", divisao);
+			}
+		}
+		
+		sc.close();
+
+	}
+
+}
